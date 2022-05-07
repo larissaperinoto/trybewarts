9f8f9d08cd7c8c25635d290b8331d09ad716d2b7
Larissa Perinoto <perinotolarissa@gmail.com>
2022-05-07 18:21:38 -0300

    Requisito 21

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function fazerLogin() {
-  const email = document.getElementById('email');
+  const login = document.getElementById('email');
   const senha = document.getElementById('senha');
-  if (email.value === 'tryber@teste.com' && senha.value === '123456') {
+  if (login.value === 'tryber@teste.com' && senha.value === '123456') {
     alert('Olá, Tryber!');
   } else {
     alert('Email ou senha inválidos.');
@@ -12,9 +12,9 @@ const botaoEntrar = document.getElementById('botao-entrar');
 botaoEntrar.addEventListener('click', fazerLogin);
 
 const agreement = document.getElementById('agreement');
+const botaoEnviar = document.getElementById('submit-btn');
 
 function habilitar() {
-  const botaoEnviar = document.getElementById('submit-btn');
   if (agreement.checked === true) {
     botaoEnviar.disabled = false;
   } else {
@@ -37,3 +37,62 @@ function contadorLetras(event) {
 }
 
 textarea.addEventListener('keyup', contadorLetras);
+
+const nome = document.getElementById('input-name');
+const sobrenome = document.getElementById('input-lastname');
+const email = document.getElementById('input-email');
+const casa = document.getElementById('house');
+const avaliacao = document.getElementsByClassName('avaliacao');
+const familia = document.getElementsByClassName('familia');
+const conteudo = document.getElementsByClassName('subject');
+
+function inputFamilia() {
+  for (const item of familia) {
+    if (item.checked === true) {
+      const valorFamilia = item.value;
+      return valorFamilia;
+    }
+  }
+}
+
+function inputConteudo() {
+  const valorConteudo = [];
+  for (const item of conteudo) {
+    if (item.checked === true) {
+      valorConteudo.push(` ${item.value}`);
+    }
+  }
+  return valorConteudo;
+}
+
+function inputAvaliacao() {
+  for (const item of avaliacao) {
+    if (item.checked === true) {
+      const valorAvaliacao = item.value;
+      return valorAvaliacao;
+    }
+  }
+}
+
+function dadosUsuario() {
+  const formulario = document.getElementById('evaluation-form');
+  const dados = document.createElement('p');
+  dados.innerText = `Nome:  ${nome.value} ${sobrenome.value}
+    Email: ${email.value}
+    Casa: ${casa.value}
+    Família: ${inputFamilia()}
+    Matérias: ${inputConteudo()}
+    Avaliação: ${inputAvaliacao()}
+    Observações: ${textarea.value}`;
+  formulario.appendChild(dados);
+}
+
+function escondeForms() {
+  const conteudoForms = document.getElementsByTagName('div');
+  for (const element of conteudoForms) {
+    element.classList.add('hide');
+  }
+  dadosUsuario();
+}
+
+botaoEnviar.addEventListener('click', escondeForms);
