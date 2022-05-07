34fd949b87b3471fea40214f4734f465430d6d87
heloisasantana <heloisasantana.silva@hotmail.com>
2022-05-07 11:02:36 -0300

    Requisito 3

---
+++ script.js
@@ -0,0 +1,12 @@
+function fazerLogin() {
+  const email = document.getElementById('email');
+  const senha = document.getElementById('senha');
+  if (email.value === 'tryber@teste.com' && senha.value === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Email ou senha inválidos.');
+  }
+}
+
+const botaoEntrar = document.getElementById('botao-entrar');
+botaoEntrar.addEventListener('click', fazerLogin);
